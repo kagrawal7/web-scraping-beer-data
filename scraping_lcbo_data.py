01bae0d8a2964755ed83147f1bc9193dc182ac2a
kagrawal7 <k.agrawal@mail.utoronto.ca>
2022-08-31 18:12:41 +0530

    created lcbo scraping file

---
+++ scraping_lcbo_data.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+# We want to create a dictionary mapping reviewers to a list of beers they
+# have left reviews for from the LCBO site
+
+if __name__ == '__main__':
+    ## run web scraper
+    x = 2
